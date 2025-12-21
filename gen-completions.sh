578052aaea0491a6528bab34b302101f3f008419
dsluo <me@dsluo.dev>
2025-12-20 19:55:58 -0500

    feat: mise tool completions (for fish at least)

---
+++ gen-completions.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+TARGET_SHELL="${1:-$(basename $SHELL)}"
+COMPLETIONS_DIR="./.completions/${TARGET_SHELL}"
+mkdir -p $COMPLETIONS_DIR
+
+if [[ "$TARGET_SHELL" -eq "fish" ]]; then
+    COMPLETION_EXT="fish"
+elif [[ "$TARGET_SHELL" -eq "zsh" ]]; then
+    COMPLETION_EXT="zsh"
+elif [[ "$TARGET_SHELL" -eq "pwsh" ]]; then
+    COMPLETION_EXT="ps1"
+else
+    COMPLETION_EXT="sh"
+fi
+
+# this ensures that we only get the tools defined in the local config.
+TOOLS=$(mise config get tools | cut -d'"' -f2)
+
+for TOOL in $TOOLS; do
+    BINPATH="$(mise bin-paths $TOOL)"
+
+    for ITEM in $BINPATH/*; do
+
+        if [[ -f "$ITEM" && -x "$ITEM" ]]; then
+            EXEC_NAME=$(basename $ITEM)
+            
+            # try to generate completion
+            COMPLETION="$($ITEM completion $TARGET_SHELL 2>/dev/null)"
+            if [[ "$?" -ne 0 ]]; then
+
+                # try again with --completion; Task does it this way.
+                COMPLETION="$($ITEM --completion $TARGET_SHELL 2>/dev/null)"
+                if [[ "$?" -ne 0 ]]; then
+                    # give up
+                    continue
+                fi
+            fi
+
+            echo "$COMPLETION" > $COMPLETIONS_DIR/$EXEC_NAME.$COMPLETION_EXT
+        fi
+    done
+done
