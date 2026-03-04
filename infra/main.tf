8f3320900aed9ecfbf8fa31769b75d6112933810
dsluo <me@dsluo.dev>
2026-03-04 18:09:51 -0500

    feat: terragrunt

--- infra/main.tf
+++
@@ -1,8 +0,0 @@
-module "sw_core" {
-  source = "./sw_core"
-}
-
-module "backblaze" {
-  source             = "./backblaze"
-  backup_bucket_name = var.backup_bucket_name
-}
--- infra/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "backup_bucket_name" {
-  type = string
-}
